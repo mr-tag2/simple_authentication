34ca678d5f1f4ae71ae01184905bab07d06bf3d9
mr_tag2 <m.taji1381@gmail.com>
2022-07-06 16:22:54 +0430

    Publishing Project

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,11 +3,11 @@ import { reducers } from "./../reducers";
 import thunk from "redux-thunk";
 
 export const store = createStore(
-    reducers,
-    compose(
-        applyMiddleware(thunk)
-        , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  reducers,
+  compose(
+    applyMiddleware(thunk)
+    // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 //subscribe
